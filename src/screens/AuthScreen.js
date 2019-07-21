bb8c0f89351c3331b6d0c997df7dd846f5f2bd21
Nick Rameau <r@meau.me>
2019-07-21 10:09:56 -0400

    Improve auth screen

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import {
   TextInput,
   withTheme,
-  Button
+  Button,
+  Surface,
 } from 'react-native-paper';
+import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
+import { Dropdown } from 'react-native-material-dropdown';
+import { handleLogin } from '../store/actions/AuthActions';
+import KeyboardAwareView from '../components/utils/KeyboardAwareView';
+
+const width = Dimensions.get('window').width;
+
+const sitesData = [
+  { value: 'Display2' },
+  { value: 'Display1' },
+  { value: 'Headline' },
+  { value: 'Title' },
+  { value: 'Subheading' },
+  { value: 'Body' },
+  { value: 'Caption' },
+];
 
 class AuthScreen extends React.Component {
-  static navigationOptions = {
-    headerTitle: 'Version 1.0.0',
-    headerLeft: (
-      <Image source={require('../../assets/swe-logo.png')} resizeMode='stretch' style={{
-        width: 50,
-        height: 50,
-        left: 5,
-      }} />
-    ),
-  };
-
   constructor(props) {
     super(props);
 
@@ -25,87 +38,107 @@ class AuthScreen extends React.Component {
       serviceUrl: null,
       site: null,
       usernameOrEmail: null,
-      password: null
-    }
+      password: null,
+      loading: false,
+      isLoggedIn: false,
+      selectedSite: 'Subheading',
+    };
+
+    this._handleLogin = this._handleLogin.bind(this);
+    this._onSiteSelected = this._onSiteSelected.bind(this);
+  }
+
+  _handleLogin() {
+    this.setState({ loading: true });
+
+    setTimeout(() => {
+      this.setState({
+        loading: false,
+        isLoggedIn: true
+      });
+    }, 4000);
+    // this.props.handleLogin(this.state);
+  }
+
+  _onSiteSelected(selectedSite) {
+    this.setState({ selectedSite })
   }
 
   render() {
-    const { colors } = this.props.theme;
+    const { colors, roundness } = this.props.theme;
 
     return (
       <View style={[styles.container, { backgroundColor: colors.background }]}>
-        <TextInput
-          style={styles.textInput}
-          label='SEMA Service URL'
-          mode="outlined"
-          placeholder="SEMA Service URL"
-          returnKeyType="next"
-          maxLength={250}
-          value={this.state.serviceUrl}
-          onChangeText={serviceUrl => { this.setState({ serviceUrl }) }}
-          onSubmitEditing={() => { this.siteInput.focus(); }}
-          blurOnSubmit={false}
-        />
+        <Surface style={styles.formContainer}>
+          {this.state.isLoggedIn ?
+            <TouchableOpacity>
+              <Dropdown
+                value={this.state.selectedSite}
+                onChangeText={this._onSiteSelected}
+                label='Select Site...'
+                data={sitesData}
+                containerStyle={[styles.dropdownContainer, { width: width * .5}]}
+              />
+            </TouchableOpacity> :
+            <React.Fragment>
+              <KeyboardAwareView style={{ justifyContent: 'center' }}>
+                <TextInput
+                  ref={(input) => { this.usernameOrEmailInput = input; }}
+                  style={[styles.textInput, { width: width * .5 }]}
+                  label='Username or Email'
+                  mode="outlined"
+                  returnKeyType="next"
+                  maxLength={250}
+                  value={this.state.usernameOrEmail}
+                  onChangeText={usernameOrEmail => { this.setState({ usernameOrEmail }) }}
+                  onEndEditing={() => { this.passwordInput.focus(); }}
+                  blurOnSubmit={false}
+                />
 
-        <TextInput
-          ref={(input) => { this.siteInput = input; }}
-          style={styles.textInput}
-          label='Site'
-          mode="outlined"
-          placeholder="Site"
-          returnKeyType="next"
-          maxLength={250}
-          value={this.state.site}
-          onChangeText={site => { this.setState({ site }) }}
-          onSubmitEditing={() => { this.usernameOrEmailInput.focus(); }}
-          blurOnSubmit={false}
-        />
+                <TextInput
+                  ref={(input) => { this.passwordInput = input; }}
+                  style={[styles.textInput, { width: width * .5 }]}
+                  label='Password'
+                  mode="outlined"
+                  returnKeyType="done"
+                  maxLength={250}
+                  value={this.state.password}
+                  onChangeText={password => { this.setState({ password }) }}
+                  secureTextEntry={true}
+                />
+              </KeyboardAwareView>
 
-        <TextInput
-          ref={(input) => { this.usernameOrEmailInput = input; }}
-          style={styles.textInput}
-          label='Username or Email'
-          mode="outlined"
-          placeholder="Username or Email"
-          returnKeyType="next"
-          maxLength={250}
-          value={this.state.usernameOrEmail}
-          onChangeText={usernameOrEmail => { this.setState({ usernameOrEmail }) }}
-          onSubmitEditing={() => { this.passwordInput.focus(); }}
-          blurOnSubmit={false}
-        />
-
-        <TextInput
-          ref={(input) => { this.passwordInput = input; }}
-          style={styles.textInput}
-          label='Password'
-          mode="outlined"
-          placeholder="Password"
-          returnKeyType="done"
-          maxLength={250}
-          value={this.state.password}
-          onChangeText={password => { this.setState({ password }) }}
-          secureTextEntry={true}
-        />
-
-        <View style={styles.authButtons}>
-          <Button mode="contained" onPress={() => console.log('Pressed')}>
-            Connect
-          </Button>
-        </View>
+              <View style={styles.authButtons}>
+                <Button
+                  mode="contained"
+                  onPress={() => this._handleLogin()}
+                  loading={this.state.loading}
+                >
+                  Login
+            </Button>
+              </View>
+            </React.Fragment>
+          }
+        </Surface>
       </View>
     );
   }
 }
 
-export default withTheme(AuthScreen);
+function mapDispatchToProps(dispatch) {
+  return {
+    handleLogin: bindActionCreators(handleLogin, dispatch),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(withTheme(AuthScreen));
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
-    padding: 200
+    alignItems: 'center',
   },
 
   authButtons: {
@@ -115,6 +148,28 @@ const styles = StyleSheet.create({
   },
 
   textInput: {
-    marginTop: 10
+    marginTop: 10,
+    backgroundColor: '#fff'
+  },
+
+  formContainer: {
+    flex: 0,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 2,
+  },
+
+  sitePicker: {
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  sitePickerText: {
+    color: '#fff',
+  },
+
+  dropdownContainer: {
   },
 });
