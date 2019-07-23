043336855c6d323b25b22422f487d9315358bc09
Nick Rameau <r@meau.me>
2019-07-23 19:15:49 -0400

    Introduce redux-persist

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { Provider as StoreProvider } from 'react-redux';
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 import store from './src/store';
 import MainNavigator from './src/navigation/MainNavigator';
+
+const persistor = persistStore(store);
 
 export const theme = {
   ...DefaultTheme,
@@ -16,9 +20,11 @@ export const theme = {
 export default function App() {
   return (
     <StoreProvider store={store}>
-      <PaperProvider theme={theme}>
-        <MainNavigator />
-      </PaperProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <PaperProvider theme={theme}>
+          <MainNavigator />
+        </PaperProvider>
+      </PersistGate>
     </StoreProvider>
   );
 }
--- src/store/reducers/RootReducer.js
+++ src/store/reducers/RootReducer.js
@@ -1,9 +1,26 @@
 import { combineReducers } from 'redux';
 import authReducer from "./AuthReducer";
+import createSecureStore from "redux-persist-expo-securestore";
+import { persistReducer } from "redux-persist";
+import ExpoFileSystemStorage from "redux-persist-expo-filesystem";
+
+// Secure storage
+const secureStorage = createSecureStore();
+
+const authPersistConfig = {
+  key: 'secure',
+  storage: secureStorage
+};
+
+const rootPersistConfig = {
+  key: 'root',
+  storage: ExpoFileSystemStorage,
+  blacklist: ['auth']
+};
 
 // Combine all the reducers
 const RootReducer = combineReducers({
-	authReducer,
+  auth: persistReducer(authPersistConfig, authReducer)
 });
 
-export default RootReducer;
+export default persistReducer(rootPersistConfig, RootReducer);
