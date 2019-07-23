fa57333303d7973a05acdbe41b156a39b2e17668
Nick Rameau <r@meau.me>
2019-07-23 19:26:01 -0400

    Introduce services

---
+++ src/services/axiosService.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import Constants from 'expo-constants';
+
+const envVar = process.env.NODE_ENV || 'development';
+
+const axiosOptions = {
+  baseURL: Constants.manifest.extra.semaServiceUrl[envVar]
+};
+
+const axiosService = axios.create(axiosOptions);
+
+// Our custom middleware to always update our axios headers with the latest
+// token. This saves us from including the token on every request.
+// In redux, a middleware is a bridge between the action creator and the reducer
+export const axiosMiddleware = ({ dispatch, getState }) => next => action => {
+  if (action.type === 'LOGIN_SUCCESS') {
+    axiosService.defaults.headers.common['Authorization'] = `Bearer ${action.data.token}`;
+  }
+
+  return next(action);
+}
+
+export default axiosService;
