ca680642f5ceb633ff4e4900345e389c728449df
Nick Rameau <r@meau.me>
2019-07-23 19:22:13 -0400

    Introduce redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,22 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { axiosMiddleware } from '../services/axiosService';
 
-import RootReducer from './reducers/RootReducer'
+import RootReducer from './reducers/RootReducer';
 
-// Connect our store to the reducers
-export default createStore(RootReducer, applyMiddleware(thunk));
+const middlewares = [
+	thunk,
+	axiosMiddleware,
+];
+
+if (process.env.NODE_ENV === 'development') {
+  const { createLogger } = require('redux-logger');
+  const logger = createLogger({
+	  collapsed: (getState, action) => action.type === 'persist/REHYDRATE' || action.type === 'persist/PERSIST',
+	  diff: true,
+  })
+
+  middlewares.push(logger);
+}
+
+export default createStore(RootReducer, applyMiddleware(...middlewares));
